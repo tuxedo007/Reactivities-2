6394b5b7f75d948c260ae9c5f216d3ea3bf6ba91
Gary <mailbox313@hotmail.com>
2020-02-21 13:26:52 -0500

    End of section 16

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,10 +9,11 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        
+
         public DbSet<Value> Values { get; set; }
         public DbSet<Activity> Activities { get; set; }
         public DbSet<UserActivity> UserActivities { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -20,11 +21,11 @@ namespace Persistence
 
             builder.Entity<Value>()
                 .HasData(
-                    new Value {Id = 1, Name = "Value 101"},
-                    new Value {Id = 2, Name = "Value 102"},
-                    new Value {Id = 3, Name = "Value 103"}
+                    new Value { Id = 1, Name = "Value 101" },
+                    new Value { Id = 2, Name = "Value 102" },
+                    new Value { Id = 3, Name = "Value 103" }
                 );
-                
+
             builder.Entity<UserActivity>(x => x.HasKey(ua =>
                 new { ua.AppUserId, ua.ActivityId }));
 
