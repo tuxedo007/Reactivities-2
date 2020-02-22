f8e8dd0bbc9c4b9f762d893c26f5420fa7070114
Gary <mailbox313@hotmail.com>
2020-02-22 16:00:22 -0500

    Section 17 react-cropper problem

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Item, Button, Segment, Icon, Label } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { IActivity } from '../../../app/models/activity';
-import {format} from 'date-fns';
+import { format } from 'date-fns';
 import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
@@ -12,16 +12,26 @@ const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' circular src={host.image || '/assets/user.png'} />
+            <Item.Image
+              size="tiny"
+              circular
+              src={host.image || '/assets/user.png'}
+              style={{ marginBottom: 3 }}
+            />
             <Item.Content>
-              <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
-              <Item.Description>Hosted by {host.displayName}</Item.Description>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>
+                Hosted by{' '}
+                <Link to={`/profile/${host.username}`}>{host.displayName}</Link>
+              </Item.Description>
               {activity.isHost && (
                 <Item.Description>
                   <Label
                     basic
-                    color='orange'
-                    content='You are hosting this activity'
+                    color="orange"
+                    content="You are hosting this activity"
                   />
                 </Item.Description>
               )}
@@ -29,8 +39,8 @@ const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
                 <Item.Description>
                   <Label
                     basic
-                    color='green'
-                    content='You are going to this activity'
+                    color="green"
+                    content="You are going to this activity"
                   />
                 </Item.Description>
               )}
@@ -39,18 +49,20 @@ const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
         </Item.Group>
       </Segment>
       <Segment>
-        <Icon name='clock' /> {format(activity.date, 'h:mm a')}
-        <Icon name='marker' /> {activity.venue}, {activity.city}
+        <Icon name="clock" /> {format(activity.date, 'h:mm a')}
+        <Icon name="marker" /> {activity.venue}, {activity.city}
       </Segment>
-      <Segment secondary><ActivityListItemAttendees attendees={activity.attendees}/></Segment>
+      <Segment secondary>
+        <ActivityListItemAttendees attendees={activity.attendees} />
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
           as={Link}
           to={`/activities/${activity.id}`}
-          floated='right'
-          content='View'
-          color='blue'
+          floated="right"
+          content="View"
+          color="blue"
         />
       </Segment>
     </Segment.Group>
